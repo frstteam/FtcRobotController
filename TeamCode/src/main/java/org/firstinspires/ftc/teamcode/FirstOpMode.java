4189142fb4dc4530f44a4897bed03816c9feb456
Vandan Juvekar <frstteam@gmail.com>
2023-10-25 19:29:59 -0400

    Initial attempts using samples

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+/**
+ * First test op mode
+ */
+@TeleOp
+public class FirstOpMode extends LinearOpMode {
+    private DcMotor revCoreHexMotor;
+    private RevColorSensorV3 revColorSensorV3;
+    private RevTouchSensor revTouchSensor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        revCoreHexMotor = hardwareMap.get(DcMotor.class, "revCoreHexMotor");
+        revColorSensorV3 = hardwareMap.get(RevColorSensorV3.class, "revColorSensorV3");
+        revTouchSensor = hardwareMap.get(RevTouchSensor.class, "revTouchSensor");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        double targetPower = 0;
+        double targetPosition = 0;
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Color - Light", revColorSensorV3.getLightDetected());
+            telemetry.addData("Color - Distance (mm)", revColorSensorV3.getDistance(DistanceUnit.MM));
+            telemetry.addData("Touch - Value", revTouchSensor.getValue());
+            telemetry.addData("Touch - Pressed", revTouchSensor.isPressed());
+
+            telemetry.addLine();
+
+            targetPower = -gamepad1.left_stick_y;
+            targetPosition = gamepad1.right_stick_x;
+            if (Math.abs(targetPower) > 0) {
+                revCoreHexMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                revCoreHexMotor.setPower(targetPower);
+            }
+            else if (Math.abs(targetPosition) > 0) {
+                revCoreHexMotor.setPower(0.25);
+                revCoreHexMotor.setTargetPosition(revCoreHexMotor.getCurrentPosition() + (int) targetPosition * 100);
+                revCoreHexMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+
+            telemetry.addData("Target Power", targetPower);
+            telemetry.addData("Motor Power", revCoreHexMotor.getPower());
+            telemetry.addData("Target Position", targetPosition);
+            telemetry.addData("Motor Position", revCoreHexMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -6,14 +6,12 @@ This module, TeamCode, is the place where you will write/paste the code for your
 robot controller App. This module is currently empty (a clean slate) but the
 process for adding OpModes is straightforward.
 
-## Creating your own OpModes
+## Creating our own OpModes
 
-The easiest way to create your own OpMode is to copy a Sample OpMode and make it your own.
+Currently copying a few Sample OpModes and making it our own.
 
 Sample opmodes exist in the FtcRobotController module.
-To locate these samples, find the FtcRobotController module in the "Project/Android" tab.
-
-Expand the following tree elements:
+Expanded the following tree elements:
  FtcRobotController/java/org.firstinspires.ftc.robotcontroller/external/samples
 
 ### Naming of Samples
